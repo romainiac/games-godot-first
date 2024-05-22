bcb469d89c71a2283bc73e39b45c2ae10582ab14
Roman Yefimets <e-man@contactroman.com>
2024-05-22 09:21:48 -0700

    adding run animation, chaning project physics fps to reduce jitter, making sure character can still jump .1 seconds after leaving the ground

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,15 +7,21 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var timer = $Timer
 
 
 func _physics_process(delta):
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if is_on_floor():
+		timer.start()
+	
+	print(timer.time_left)
+	if Input.is_action_just_pressed("jump") and timer.time_left > 0:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction: -1, 0, 1
@@ -24,6 +30,14 @@ func _physics_process(delta):
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
+		
+	# Play animation
+	if direction == 0:
+		animated_sprite.play("idle")
+	else:
+		animated_sprite.play("run")
+		
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
